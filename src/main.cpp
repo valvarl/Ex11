5cada58a79ccb2f58a6219981451422db8bd39a9
anton <ashtanyuk@gmail.com>
2021-04-04 20:22:14 +0300

    Начальный коммит

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// Copyright 2021
+#include <iostream>
+#include <string>
+
+#include "train.h"
+
+int main() {
+  return 0;
+}
+
---
+++ test/tests.cpp
@@ -0,0 +1,7 @@
+// Copyright 2021 GHA Test Team
+#include <gtest/gtest.h>
+#include <string>
+#include "train.h"
+
+
+
